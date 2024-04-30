1312091f487725db1a33d8003789a8eb054ca877
Victor Sakamoto <victorysyama@gmail.com>
2024-04-30 13:46:53 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Set<Product> set = new HashSet<>();
+		
+		set.add(new Product("TV", 900.00));
+		set.add(new Product("Notebook", 1200.00));
+		set.add(new Product("Tablet", 400.00));
+		
+		Product prod = new Product("Notebook", 1200.00);
+		
+		System.out.println(set.contains(prod));
+
+	}
+
+}
