5506e1077fc324423853078e8e326d6c7649a00f
Victor Sakamoto <victorysyama@gmail.com>
2024-04-30 13:59:52 -0300

    treeSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,20 @@ public class Program {
 		
 		Product prod = new Product("Notebook", 1200.00);
 		
+		System.out.println("Set:");
 		System.out.println(set.contains(prod));
+		
+		
+		System.out.println("\nTreeset:");
+		
+		Set<Product> treeSet = new HashSet<>();
+		
+		treeSet.add(new Product("TV", 900.00));
+		treeSet.add(new Product("Notebook", 1200.00));
+		treeSet.add(new Product("Tablet", 400.00));
+		for (Product p : treeSet) {
+			System.out.println(p);
+		}
 
 	}
 
